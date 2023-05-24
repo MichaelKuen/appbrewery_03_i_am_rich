b1de075b0ffe6fae4e35cf00cc898f635fa9863b
MichaelKuen <34417467+MichaelKuen@users.noreply.github.com>
2023-05-24 22:24:24 +0200

    Removed Code from Previous Lesson. New Code added, Scaffold, AppBar with Title and background colour, Scaffold Background colour, Network Image that is centerd.

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,19 @@ import 'package:flutter/material.dart';
 //The Main Function is the starting point for all our Flutter Apps
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U'),
+          ),
+        ),
       ),
     ),
   );
